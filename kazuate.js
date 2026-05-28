a0abf45b6412883b7d1bfe5ba0eca4c590a5915c
G484392024 <248439@st.takushoku-u.ac.jp>
2026-05-28 15:29:14 +0900

    変更

--- kazuate.js
+++ kazuate.js
@@ -12,38 +12,36 @@ let t = false;
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
+kaisu++
 let spankaisu = document.querySelector('span#kaisu');
-let spananswer = document.querySelector('span#answer');
 spankaisu.textContent = kaisu;
   // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
   
   // ここから: テキストボックスに指定された数値を yoso に代入する
-  let kazu = document.querySelector('input#kazu');
+  let kazu = document.querySelector('input#left');
   let yoso = Number(kazu.value);
-  spananswer.textContent = yoso;
   // ここまで: テキストボックスに指定された数値を yoso に代入する
-  let spanAnswer = document.querySelector('span#answer');
   // ここから: 正解判定する
   // 　　　　  正解/不正解のときのメッセージを表示する
  let presult = document.querySelector('p#result');
-  if(t || kaisu >= 4){
+  if(t || kaisu > 3){
     presult.textContent ='答えは' + kotae + 'でした。すでにゲームは終わっています。';
   } else if(yoso === kotae) {
     presult.textContent ='正解です。おめでとう！';
     t = true;
   } else {
     if(kaisu === 3){
-      presult.textContent ='まちがいです、残念でした答えは'+ kotae + 'です。';
+      presult.textContent ='まちがい、残念でした答えは'+ kotae + 'です。';
     } else if(yoso < kotae) {
-      presult.textContent ='まちがいです、答えはもっと大きいですよ。';
+      presult.textContent ='まちがい、答えはもっと大きいですよ。';
     } else if(yoso > kotae){
-      presult.textContent ='まちがいです、答えはもっと小さいですよ。';
+      presult.textContent ='まちがい、答えはもっと小さいですよ。';
     }
   }
   // ここまで: 正解判定する
  } 
 
 // ここから: ボタンを押した時のイベントハンドラとして hantei を登録
-let bottan = document.querySelector('#print');
+let bottan = document.querySelector('#calc');
 bottan.addEventListener('click', hantei);
 // ここまで: ボタンを押した時のイベントハンドラとして hantei を登録
